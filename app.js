0a0aa16baefa52d9b008a4ad915658245d60847c
andre_falvesp <andre.paschoal@mercadolivre.com>
2021-12-05 12:52:18 -0300

    bd

--- app.js
+++ app.js
@@ -29,7 +29,9 @@ const {Pool} = require('pg');
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL || connectionString,
 
-    ssl: !!process.env.DATABASE_URL
+    ssl: {
+        rejectUnauthorized: false
+    }
 
 });
 
