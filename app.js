31e3f1b1a829e7c35f44793bb2bcd47a9939e201
andre_falvesp <andre.paschoal@mercadolivre.com>
2021-12-05 14:12:28 -0300

    Pontuação

--- app.js
+++ app.js
@@ -76,13 +76,16 @@ app.get('/user/review', async (req, res) => {
             SELECT u.iduser      as iduser,
                    u.email       as email,
                    sum(v.answer) as sumanswer,
-                   sum(s.score)  as sumscore
+                   sum(s.score)  as sumscore, 
+                   t.score       as score
             FROM user1 as u
                      natural join view1 as v
+                     inner join team as t
+                                on u.idteam = t.idteam
                      inner join score as s
                                 on v.answer = s.numanswer
             where iduser = $1
-            group by u.iduser, u.email`;
+            group by u.iduser, u.email, t.score`;
 
         const result = await client
             .query(query, [iduser]);
@@ -287,174 +290,6 @@ app.post('/sql', async (req, res) => {
                     ,selectionpt
                     ,operation
                     ,sql]);
-
-        res.send(JSON.stringify(result));
-
-    } catch (err) {
-        console.error(err);
-        res.send("Error " + err);
-    } finally {
-        client.release();
-    }})
-
-
-////////////////////////////////////////QA///////////////////////////////////////////////////////////////
-app.post('/question-answer', async (req, res) => {
-    const client = await pool.connect();
-    try {
-        const {questionen, answeren, questionpt, answerpt, iduser, idarticle} = req.body;
-        const query1 = `
-            INSERT INTO questionanswer(questionen, answeren, questionpt, answerpt,
-                                       date, iduser, idarticle)
-            VALUES ($1, $2, $3, $4,
-                    (SELECT CURRENT_DATE), $5, $6); `;
-
-        const result = await client
-            .query(query1,
-                [questionen, answeren, questionpt, answerpt,
-                    iduser, idarticle]);
-
-        res.send(JSON.stringify(result));
-
-    } catch (err) {
-        console.error(err);
-        res.send("Error " + err);
-    } finally {
-        client.release();
-    }})
-
-app.put('/question-answer', async (req, res) => {
-    const client = await pool.connect();
-    try {
-        const {questionen, answeren, questionpt, answerpt, iduser, idarticle, idqa} = req.body;
-        const query1 = `
-            INSERT INTO public.questionanswer(questionen,
-                                              answeren,
-                                              questionpt,
-                                              answerpt,
-                                              idqa,
-                                              date,
-                                              iduser,
-                                              idarticle)
-            VALUES ($1, $2, $3, $4,
-                    $7, (SELECT CURRENT_DATE), $5, $6)
-            ON CONFLICT (idqa) DO UPDATE SET (
-                                              questionen,
-                                              answeren,
-                                              questionpt,
-                                              answerpt
-                                                 )=
-                                                 (EXCLUDED.questionen,
-                                                  EXCLUDED.answeren,
-                                                  EXCLUDED.questionpt,
-                                                  EXCLUDED.answerpt)
-            WHERE questionanswer.iduser = $5
-              and questionanswer.idarticle = $6
-              and questionanswer.idqa = $7;`
-
-        const result = await client
-            .query(query1,
-                [questionen, answeren, questionpt, answerpt,
-                    iduser, idarticle, idqa]);
-
-        res.send(JSON.stringify(result));
-
-    } catch (err) {
-        console.error(err);
-        res.send("Error " + err);
-    } finally {
-        client.release();
-    }})
-
-app.get('/question-answer', async (req, res) => {
-    const client = await pool.connect();
-    try {
-        const {iduser, idqa, idarticle} = req.query;
-        const query = `
-            SELECT *
-            FROM questionanswer
-            WHERE iduser = $1
-              and idqa = $2
-              and idarticle = $3
-            order by idqa;`;
-
-        const result = await client
-            .query(query,
-                [iduser, idqa, idarticle]);
-
-        res.send(JSON.stringify(result));
-
-    } catch (err) {
-        console.error(err);
-        res.send("Error " + err);
-    } finally {
-        client.release();
-    }})
-
-app.delete('/question-answer', async (req, res) => {
-    const client = await pool.connect();
-    try {
-        const {iduser, idqa, idarticle} = req.body;
-        const query = `
-            DELETE
-            FROM questionanswer
-            WHERE iduser = $1
-              and idqa = $2
-              and idarticle = $3`;
-
-        const result = await client
-            .query(query,
-                [iduser, idqa, idarticle]);
-
-        res.send(JSON.stringify(result));
-
-    } catch (err) {
-        console.error(err);
-        res.send("Error " + err);
-    } finally {
-        client.release();
-    }})
-
-app.get('/question-answer/article', async (req, res) => {
-    const client = await pool.connect();
-    try {
-        const {iduser, idarticle} = req.query;
-        const query = `
-            SELECT *
-            FROM questionanswer
-            WHERE iduser = $1
-              and idarticle = $2
-            order by idqa;`;
-
-        const result = await client
-            .query(query,
-                [iduser, idarticle]);
-
-        res.send(JSON.stringify(result));
-
-    } catch (err) {
-        console.error(err);
-        res.send("Error " + err);
-    } finally {
-        client.release();
-    }})
-
-app.get('/question-answer/article/all', async (req, res) => {
-    const client = await pool.connect();
-    try {
-        const {iduser} = req.query;
-        const query = `
-            SELECT distinct q.idarticle,
-                            replace(a.title, '<br>', ' ') as title
-                            
-            FROM questionanswer as q
-                     inner join article as a
-                                on a.idarticle = q.idarticle
-            WHERE iduser = $1`;
-
-        const result = await client
-            .query(query,
-                [iduser]);
 
         res.send(JSON.stringify(result));
 
